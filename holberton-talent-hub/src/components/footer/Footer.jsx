7b34270c8dffe35cf9a834b9501e6bdf62aa4106
rmbodegalibre <rmbodegalibre@gmail.com>
2023-02-13 15:21:29 -0500

    Improved Footer Logo

--- holberton-talent-hub/src/components/footer/Footer.jsx
+++ holberton-talent-hub/src/components/footer/Footer.jsx
@@ -1,10 +1,10 @@
 import { FooterWrapper } from "./Footer.styles";
-import logoHolberton from "../../images/hbtnbycd_logo.fw.png"
+import logoHb from "../../images/logohb.fw.png"
 
 export const Footer = () => {
   return (
     <FooterWrapper>
-      <div className="redes"><img src={logoHolberton} className="logo" alt="Logo" /></div>
+      <div className="redes"><img src={logoHb} className="logo" alt="Logo" /></div>
     </FooterWrapper>
   );
 };
