fea8c7eed8c4b8858b68a9ebd16ee3fc09836110
rmbodegalibre <98629685+rmbodegalibre@users.noreply.github.com>
2023-02-13 00:18:02 -0500

    Changed background color
    This update is made for test and update changes in local repo

--- holberton-talent-hub/src/components/slides/Jobs.styles.jsx
+++ holberton-talent-hub/src/components/slides/Jobs.styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const SlideWrapper = styled.section`
     & .job-slide {
-    background-color: #252a2e;
+    background-color: #333333;
     width 250px;
     }
 `;
