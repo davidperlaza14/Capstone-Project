a0095fbccfe533cc569485b613a151e20b45b5f2
AvelinoC5 <avelino_63@hotmail.com>
2023-02-15 07:08:41 -0500

    update job.style - job

--- holberton-talent-hub/src/components/slides/Jobs.jsx
+++ holberton-talent-hub/src/components/slides/Jobs.jsx
@@ -1,29 +1,53 @@
-import ecommerce from '../../images/ecommerce.jpg'
-import talent1 from '../../images/talent01.jpg'
-import talent2 from '../../images/talent02.jpg'
-import talent3 from '../../images/talent03.jpg'
-import { SlideWrapper } from './Jobs.styles'
-
+import pro from "../../images/pro.jpg";
+import pro1 from "../../images/pro1.jpg";
+import pro2 from "../../images/pro2.jpg";
+import talent1 from "../../images/talent01.jpg";
+import talent2 from "../../images/talent02.jpg";
+import talent3 from "../../images/talent03.jpg";
+import { SlideWrapper } from "./Jobs.styles";
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from "react-image-gallery";
 
 export const Jobs = () => {
+  const images = [
+    {
+      original: pro,
+      thumbnail: pro,
+      originalHeight: "500px",
+      originalWidth: "300px",
+    },
+    {
+      original: pro1,
+      thumbnail: pro1,
+      originalHeight: "500px",
+      originalWidth: "300px",
+    },
+    {
+      original: pro2,
+      thumbnail: pro2,
+      originalHeight: "500px",
+      originalWidth: "300px",
+    },
+  ];
+
   return (
     <SlideWrapper>
       <div className="job-slide">
         <div className="job-container">
-
-          <img className="job-img" src={ecommerce} alt="ecommerce" />
-          <h1>ShopEase E-commerce Platform</h1>
+          <div className="carrusel1">
+            <ImageGallery items={images} />
+          </div>
+          <h1>App BuyEasy</h1>
           <p>
-            ShopEase is an e-commerce app developed by three students.
-            It offers a user-friendly interface and a personalized recommendations
-            system powered by AI. Furthermore, ShopEase seamlessly integrates with
-            a variety of shipping and delivery providers, making product delivery
-            easy for users. In summary, ShopEase is an impressive example of what
-            can be achieved when young talents collaborate on an ambitious project.
+            Buy easy, live easy with BuyEasy. Discover our innovative app that
+            takes you to a world of products and services designed for young
+            people like you. From clothing and accessories to cutting-edge
+            technology, at BuyEasy you will find everything you need to enjoy
+            your life to the fullest. Are you ready to buy easy and live easy?
+            Download BuyEasy now and join our community of young people
+            passionate about life.
           </p>
         </div>
-
-
       </div>
 
       <section>
@@ -31,31 +55,36 @@ export const Jobs = () => {
           <div className="talents">
             <img src={talent1} className="talent-img" alt="talent1" />
             <h2>Kofi Kaba</h2>
-            <p>Kofi is a dynamic African developer who always brings his A-game
+            <p>
+              Kofi is a dynamic African developer who always brings his A-game
               to the table. He has a deep understanding of C, C++, and Python,
               and is highly skilled in the use of HTML, JavaScript, and React.
               With his technical prowess and innovative approach, Kofi
-              consistently delivers top-notch results.</p>
+              consistently delivers top-notch results.
+            </p>
           </div>
           <div className="talents">
             <img src={talent2} className="talent-img" alt="talent2" />
             <h2>Ming Nakamura</h2>
-            <p>Ming is a highly skilled Asian developer who is well-versed in a wide
-              range of programming languages. He excels in C, C++, and Python, and
-              has extensive experience with HTML, JavaScript, and React. Ming's
-              dedication to his craft and commitment to quality make him a valuable
-              contributor to the team.</p>
+            <p>
+              Ming is a highly skilled Asian developer who is well-versed in a
+              wide range of programming languages. He excels in C, C++, and
+              Python, and has extensive experience with HTML, JavaScript, and
+              React. Ming's dedication to his craft and commitment to quality
+              make him a valuable contributor to the team.
+            </p>
           </div>
 
           <div className="talents">
             <img src={talent3} className="talent-img" alt="talent3" />
             <h2>Astrid Jensen</h2>
-            <p>Astrid is a brilliant Nordic developer who has a passion for coding.
-              She specializes in a variety of programming languages including C,
-              C++, and Python. Astrid's expertise in HTML, JavaScript, and React
-              has made her a valuable asset to the team.</p>
+            <p>
+              Astrid is a brilliant Nordic developer who has a passion for
+              coding. She specializes in a variety of programming languages
+              including C, C++, and Python. Astrid's expertise in HTML,
+              JavaScript, and React has made her a valuable asset to the team.
+            </p>
           </div>
-
         </div>
       </section>
     </SlideWrapper>
--- holberton-talent-hub/src/components/slides/Jobs.styles.jsx
+++ holberton-talent-hub/src/components/slides/Jobs.styles.jsx
@@ -4,45 +4,44 @@ export const SlideWrapper = styled.section`
 & {
     background: #1e2326;
     margin: auto;
+    padding: 0 10px;
 }
 
-& .job-slide {
-  padding: 20px;    
-  background-color: #1e2326;
-  max-width: 90%;
-  margin: auto;
-  text-align: center;
-}
 
 & .job-container {
   padding: 20px;    
-  background-color: #181818;
-  max-width: 90%;
+  background-color: #1e2326;
+  max-width: 100%;
   margin: auto;
   text-align: center;
   color: white;
 }
 
-& .job-img {
-  max-width: 90%;
-  box-shadow: 0 3px 8px 8px rgb(255 255 255 / 50%);
-
-}
 
 & h1 {
   align: center;
   color: #8beeee;
+  padding: 10px;
+  font-size: 50px;
+}
+
+& .job-container p {
+  align: center;
+  color: #white;
+  font-size: 20px;
+  
 }
 
 & .talent-container {
   margin: auto;
   max-width: 90%;
-  border: 1px solid black;
-  background: #515151;
+  /*border: 1px solid black;*/
+  /*background: #515151;*/
   padding: 20px;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  border-radius: 20px;
   justify-content: space-around;
   text-align: center;
 }
@@ -56,7 +55,7 @@ export const SlideWrapper = styled.section`
   font-family: 'Inconsolata';
   font-weight: bold;
   border-radius: 20px;
-  /*line-height: 120px;*/
+  line-height: 20px;
   text-align: center;
   flex-grow: 1;  
 }
