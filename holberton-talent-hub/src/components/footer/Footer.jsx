b71f296705a9fac1ea35c880b4010aacd20093e8
davidperlaza14 <davidperlaza14@gmail.com>
2023-02-16 09:21:24 -0500

    clean

--- holberton-talent-hub/src/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { FooterWrapper } from "./Footer.styles";
-
-export const Footer = () => {
-  return (
-    <FooterWrapper>
-      <div className="redes">{/*         <img src={logohbtn} className="logo" alt="Logo" /> */}</div>
-    </FooterWrapper>
-  );
-};
--- holberton-talent-hub/src/pages/Talent.jsx
+++
@@ -1,8 +0,0 @@
-export const Talent = () => {
-  const indexLoad = JSON.parse(localStorage.getItem('coderise-talent:index-load'));
-  return (
-    <>
-    <h1>Hello</h1>
-    </>
-  );
-};
