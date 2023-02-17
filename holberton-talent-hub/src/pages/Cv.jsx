373c850e10788c888a079992e291bfe928ddfc6a
rmbodegalibre <rmbodegalibre@gmail.com>
2023-02-17 15:51:15 -0500

    Added code made by David for CV Page

--- holberton-talent-hub/src/pages/Cv.jsx
+++ holberton-talent-hub/src/pages/Cv.jsx
@@ -1,25 +1,34 @@
-export const CvPage = ({ userId }) => {
+import { Home } from "../components/home/Home";
+import { About } from "../components/about/About";
+import { Services } from "../components/services/Services";
+import { Qualification } from "../components/qualification/Qualification";
+import { Work } from "../components/work/Work";
+import { Contact } from "../components/contact/Contact";
+
+/*import { Skill } from "../components/skill/Skill";*/
+
+/* export const CvPage = ({ userId }) => { */
+
+export const CvPage = () => {
   return (
     <>
       <section id="inicio" className="inicio">
-        {userId ? (
-          <div className="contenido-banner">
-            <h1>CV of {userId}</h1>
-            <p>
-              Is an cv of {userId} designed by Coderise to facilitate the search for human talent with extensive management of tools used in
-              Software development and Soft Skills. Companies and recruiters interested in hiring staff for their work teams will be able to
-            </p>
-            <div className="contenedor-img" />
-          </div>
-        ) : (
-          <div className="contenido-banner">
-            <h1>CV</h1>
-            <p>
-              Is an application designed by Coderise to facilitate the search for human talent with extensive management of tools used in
-              Software development and Soft Skills. Companies and recruiters interested in hiring staff for their work teams will be able to
-            </p>
-          </div>
-        )}
+        {/* {userId} ? ( 
+        <h1>CV of {userId}</h1>*/}
+        <Home />
+        <About />
+        {/*<Skill />*/}
+        <Services />
+        <Qualification />
+        <Contact />
+        <Work />
+        {/* ) : ( */}
+        <h1>CV</h1>
+        <p>
+          Is an application designed by Coderise to facilitate the search for human talent with extensive management of tools used in
+          Software development and Soft Skills. Companies and recruiters interested in hiring staff for their work teams will be able to
+        </p>
+        {/* ) */}
       </section>
     </>
   );
