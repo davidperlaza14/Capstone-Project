d7ac3e4f0c1230e8aacab9a82d2e8a31d9672ebb
rmbodegalibre <rmbodegalibre@gmail.com>
2023-02-17 09:37:26 -0500

    Modified Index.jsx

--- holberton-talent-hub/src/pages/Index.jsx
+++ holberton-talent-hub/src/pages/Index.jsx
@@ -24,71 +24,67 @@ export const IndexWrapper = styled.section`
 *,
 *:before,
 *:after{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box; 
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
 }
 body{
- /* background-color: #151320;*/
-    
+    background-color: #151320;
 }
 div{
-  height: 350px;
-  width: 500px;
-  position: absolute;
-  transform: translate(-50%,-50%);
-  top: 40%;
-  left: 50%;
-  border-radius: 10px;
-  box-shadow: 0 20px 35px rgba(0,0,0,0.3);
-  overflow: hidden;
+    height: 250px;
+    width: 300px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 5px;
+    box-shadow: 0 20px 35px rgba(0,0,0,0.3);
+    overflow: hidden;
 }
 div:before{
-  content: "";
-  height: 300%;
-  width: 300%;
-  position: absolute;
-  background: conic-gradient(
-    #098176,
-    #2a8c82,
-    #cacaca,
-    #ffffff,
-    #1c1c1c,
-    #000000,
-    #333333,
-    #e4e4e4,
-    #7dd9c4
-  );
-  left: -100%;
-  top: -100%;
-  animation: spin 15s infinite linear;
+    content: "";
+    height: 150%;
+    width: 150%;
+    position: absolute;
+    background: conic-gradient(
+        #098176,
+        #2a8c82,
+        #cacaca,
+        #ffffff,
+        #1c1c1c,
+        #000000,
+        #333333,
+		#e4e4e4,
+		#7dd9c4
+    );
+    left: -25%;
+    top: -25%;
+    animation: spin 15s infinite linear;
 }
 @keyframes spin{
-  100%{
-    transform: rotate(-360deg);
-  }
+    100%{
+        transform: rotate(-360deg);
+    }
 }
-
-
 div:after{
-	content: "\n\n\nIs an application designed by Coderise to facilitate the search for human talent with extensive management of tools used in Software development and Soft Skills. Companies and recruiters interested in hiring staff for their work teams will be able to find the ideal staff in this application.";
+	content: "Is an application designed by Coderise to facilitate the search for human talent with extensive management of tools used in Software development and Soft Skills. Companies and recruiters interested in hiring staff for their work teams will be able to find the ideal staff in this application.";
   position: absolute;
   background-color: #1c1b29;
-  height: 80%;
+  height: 96%;
   width: 96%;
-  top: 12%;
+  top: 2%;
   left: 2%;
-  border-radius: 10px;
-  font-family: 'Inconsolata',sans-serif;
+  border-radius: 5px;
+  font-family: 'Poppins',sans-serif;
   color: #ffffff;
-  font-size: 18px;
+  font-size: 16px;
   letter-spacing: 1px;
-
-  text-align: center;
+  display: grid;
+  place-items: center;
+	text-align: center;
   line-height: 1.5;
-}
-
-`;
+}`;
 
 export const IndexPage = () => {
   localStorage.setItem('coderise-talent:index-load', true);
