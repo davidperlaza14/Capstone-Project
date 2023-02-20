378abffcc8f75fb774b300f158fa17b27973f94b
rmbodegalibre <rmbodegalibre@gmail.com>
2023-02-20 01:22:19 -0500

    Updated Pages CV & Talent

--- holberton-talent-hub/src/pages/Talent.jsx
+++ holberton-talent-hub/src/pages/Talent.jsx
@@ -1,3 +1,5 @@
+import { Holbies } from '../components/holbies/Holbies';
+
 export const TalentPage = () => {
   const indexLoad = JSON.parse(localStorage.getItem('coderise-talent:index-load'));
   return (
@@ -5,7 +7,7 @@ export const TalentPage = () => {
       <section id="inicio" className="inicio">
         <div className="contenido-banner">
           <h1>TALENT </h1>
-
+<Holbies/>
           {indexLoad ? (
             <p>Like you read in our home page, we are a team of developers and designers who are passionate about technology and we are</p>
           ) : (
