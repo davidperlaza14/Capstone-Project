f311d700cc470edd0999e1f8ff096533ca1d7c30
JuanDAC <juanavila12.0308@gmail.com>
2023-02-22 12:29:03 -0500

    [🎨] (Fix styles)

--- holberton-talent-hub/src/pages/Projects.jsx
+++ holberton-talent-hub/src/pages/Projects.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-import { Search, KeyboardArrowUp } from '@mui/icons-material/';
-
 import { ProjectItem } from '../components/project-item/ProjectItem';
+import { SearchBar } from '../components/search-bar/SearchBar';
 
 export const ProjectsPageWrapper = styled.section`
   & main {
@@ -14,20 +12,6 @@ export const ProjectsPageWrapper = styled.section`
     max-width: 90%;
   }
 
-  & .center-container {
-    margin: 0px;
-    display: flex;
-    justify-content: center;
-    padding: 10px;
-    align-items: center;
-    position: fixed;
-    width: 100%;
-    background: #2d333d;
-    z-index: 1;
-    height: 85px;
-    transition: all 0.5s;
-    box-shadow: 0px 0px 0px 1px #8beeee;
-  }
   .center-container-footer {
     margin: 0px;
     display: flex;
@@ -45,54 +29,12 @@ export const ProjectsPageWrapper = styled.section`
     border: 1px solid blue;
   }
 
-  & .input-search {
-    margin-right: 35px;
-    height: 40px;
-    padding: 5px;
-    border-radius: 5px;
-    border-color: gray;
-    border-style: double;
-    font-size: 20px;
-  }
-
   & .input-number {
     height: 40px;
     padding: 5px;
     border-radius: 5px;
     border-color: gray;
     border-style: double;
-  }
-
-  & .button-search {
-    box-shadow: 2px 2px #000;
-    padding: 10px 25px;
-    cursor: pointer;
-    border: none;
-    color: white;
-    height: 40px;
-    font-weight: 600;
-    font-size: 20px;
-    border-radius: 6px;
-    background: #098177;
-  }
-
-  & .button-search:hover {
-    background: #064f49;
-  }
-
-  & .button-activation-search {
-    position: absolute;
-    right: 20px;
-    transition: all 0.5s;
-    box-shadow: 2px 2px #000;
-    padding: 10px 25px;
-    cursor: pointer;
-    border: none;
-    color: white;
-    font-weight: 600;
-    font-size: 20px;
-    border-radius: 6px;
-    background: #098177;
   }
 
   & .button-three {
@@ -129,25 +71,11 @@ export const ProjectsPageWrapper = styled.section`
 `;
 
 export const ProjectsPage = () => {
-  const [show, setShow] = useState(false);
   const projects = [{}, {}, {}];
   return (
     <ProjectsPageWrapper>
       <div className="container-projects">
-        <div className="center-container" style={{ top: show ? '80px' : '0px' }}>
-          <input className="input-search" type="text" placeholder="Search" />
-          <button className="button-search" type="button">
-            Search
-          </button>
-          <button
-            className="button-activation-search"
-            type="button"
-            style={{ top: show ? '25px' : 'calc(100% + 20px)' }}
-            onClick={() => setShow(!show)}
-          >
-            {show ? <KeyboardArrowUp /> : <Search />}
-          </button>
-        </div>
+        <SearchBar />
         <main>
           {projects.map((project, key) => (
             <ProjectItem key={key} {...project} />
